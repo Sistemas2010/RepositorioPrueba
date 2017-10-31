3a2bc5f5b00a03fe6672045418d6172cc2acf2bb
sistemas <albertoanco.lavisa@gmail.com>
2017-10-31 12:22:06 -0500

    Se modifico el numero de caracteres del nombre en el modelo de clientes

--- apps/clientes/models.py
+++ apps/clientes/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Cliente(models.Model):
     dni = models.IntegerField(unique=True)
-    nombre = models.CharField(max_length=60)
+    nombre = models.CharField(max_length=600)
     apellidos = models.CharField(max_length=100)
     direccion = models.CharField(max_length=100)
     telefono = models.IntegerField(verbose_name='Telefono')
